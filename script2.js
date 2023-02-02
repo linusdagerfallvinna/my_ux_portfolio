bc20eb5e0ed3c4765ef08644b95c4de18428c8e9
linusdagerfallvinna <linusdv97@gmail.com>
2023-02-02 21:53:10 +0100

    thursday-changes

--- script2.js
+++ script2.js
@@ -8,40 +8,10 @@ const updateUI = (data) => {
     const weather = data.weather;
     //update details tempelete
 
-
-
-    /*details.innerHTML = `
-    <h5 class="my-3">${cityDets.EnglishName}</h5>
-        <div class="my-3">${weather.WeatherText}</div>
-        <div class="display-4 my-4">
-            <span>${weather.Temperature.Metric.Value}</span>
-            <span>&deg;C</span>
-        </div>
-    `;*/
-
     console.log(weather.WeatherText);
-    /*if(weather.WeatherText == "Sunny"){
-        document.body.style.backgroundColor = "blue"
-        console.log("Soligt");
-    }
-    else{
-        document.body.style.backgroundColor = "red"
-        console.log("Inte soligt");
-    }*/
 
-    document.querySelector("#city").innerHTML = ` ${cityDets.EnglishName.toUpperCase()} <br /> ${weather.WeatherText.toUpperCase()} <br /> ${weather.Temperature.Metric.Value}°`;
+    document.querySelector("#city").innerHTML = ` ${cityDets.EnglishName.toUpperCase()} <br /> ${weather.WeatherText.toUpperCase()} <br /> ${weather.Temperature.Metric.Value}°C`;
 
-    //Sunny
-	//Mostly Sunny
-3	//Partly Sunny
-4	//Intermittent Clouds
-5	//Hazy Sunshine
-
-
-    //Clouds and sun
-    //Mostly clear
-
-    //Mostly cloudy
     if (weather.WeatherText == "Sunny" || weather.WeatherText == "Clear" || weather.WeatherText == "Clouds and sun" || weather.WeatherText == "Clouds and sun" || weather.WeatherText == "Mostly clear" || weather.WeatherText == "Mostly sunny" || weather.WeatherText == "Hazy sunshine" || weather.WeatherText == "Partly sunny") {
         /*document.getElementById('background-wrap').style.backgroundColor = "#00b4ff";*/
         document.getElementById('background-wrap').style.background = "linear-gradient(#2BC0E4, #EAECC6)";
@@ -89,60 +59,31 @@ cityForm.addEventListener('submit', e => {
         .catch(err => console.log(err));
 })
 
-/*cityForm.addEventListener('submit', e => {
-    //prevent default action
-    e.preventDefault();
-
-    //get city value
-    const city = cityForm.city.value.trim();
-    cityForm.reset();
-
-    //Update the UI with new city
-    myFunction(city)
-})
-*/
-
-function myFunction(city) {
-    if (city == "Stockholm") {
-        /*document.getElementById('background-wrap').style.backgroundColor = "#00b4ff";*/
-        document.getElementById('background-wrap').style.backgroundImage = "linear-gradient(#2BC0E4, #EAECC6)";
-        document.querySelector("#city").innerHTML = `${city.toUpperCase()} <br /> SUNNY <br /> 10°`;
-        document.getElementById('city').style.color = "white";
-        sun()
-    }
-    else {
-        document.getElementById('background-wrap').style.backgroundColor = "#cccccc";
-        document.querySelector("#city").innerHTML = `${city.toUpperCase()} <br /> CLOUDY <br /> 15°`;
-        document.getElementById('city').style.color = "black";
-        cloud()
-    }
-}
-
 
 function sun() {
-    document.getElementById('sundiv').style.display = "block";
+    document.getElementById('sundiv').style.display = "flex";
     document.getElementById('rain').style.display = "none";
     document.getElementById('snow').style.display = "none";
-    document.getElementById('container2').style.display = "none";
+    document.getElementById('cloud').style.display = "none";
 }
 
 function snow() {
     document.getElementById('sundiv').style.display = "none";
     document.getElementById('rain').style.display = "none";
     document.getElementById('snow').style.display = "block";
-    document.getElementById('container2').style.display = "block";
+    document.getElementById('cloud').style.display = "block";
 }
 
 function cloud() {
     document.getElementById('sundiv').style.display = "none";
     document.getElementById('rain').style.display = "block";
     document.getElementById('snow').style.display = "none";
-    document.getElementById('container2').style.display = "block";
+    document.getElementById('cloud').style.display = "block";
 }
 
 function hideall() {
     document.getElementById('sundiv').style.display = "none";
     document.getElementById('snow').style.display = "none";
     document.getElementById('rain').style.display = "none";
-    document.getElementById('container2').style.display = "block";
+    document.getElementById('cloud').style.display = "block";
 }
